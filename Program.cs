b0f0527770b73f767c27de62960904704e46d15e
u <u@DESKTOP-94GCSEP>
2022-10-04 12:15:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int a = 10, b = 20;
+
+		if (a > b)
+		{
+			Console.WriteLine("a is greater than b");
+		}
+		else if (a < b)
+		{
+			Console.WriteLine("a is less than b"); 
+		}
+
+		else
+		{
+			Console.WriteLine("a is equal to b");
+        }
+
+		
+		Console.ReadLine();
+
+	}
+}
